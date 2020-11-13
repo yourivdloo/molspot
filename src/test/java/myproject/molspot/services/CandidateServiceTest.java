7a2c962324923511857e7da38b0d46a8c9729fdf
Youri <youri.yvdl@gmail.com>
2020-11-13 14:47:24 +0100

    fixed test class

--- src/test/java/myproject/molspot/services/CandidateServiceTest.java
+++ src/test/java/myproject/molspot/services/CandidateServiceTest.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 @SpringBootTest
-public class CandidateServiceTest {
+class CandidateServiceTest {
 
     @Mock
     CandidateRepository candidateRepository;
